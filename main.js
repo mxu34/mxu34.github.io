a08fc8fbfb41ca0e84fe3e90671971f9c08b6752
mengdi <mengdixu@andrew.cmu.edu>
2022-03-26 22:00:27 -0400

    update mobile nav

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
 var mobileMenu = document.querySelector(".mobile-menu");
+
 mobileMenuBtn.addEventListener("click", () => {
   if (mobileMenu.style.display === "none") {
     mobileMenu.style.display = "flex";
